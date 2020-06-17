31914688588f532f906c141f51f4ef424f0c2dba
Lee <leedror33@gmail.com>
2020-06-17 16:28:48 +0300

    weather

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ console.log('Main!');
 
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
+import { weatherService } from './services/weather.service.js'
 
 
 locService.getLocs()
@@ -29,3 +30,19 @@ document.querySelector('.btn').addEventListener('click', (ev) => {
     console.log('Aha!', ev.target);
     mapService.panTo(35.6895, 139.6917);
 })
+
+
+//lee
+
+
+weatherService.getWeatherApi(56, 43)
+    .then((data) => {
+        console.log('lee', data);
+        renderWeather(data);
+    })
+
+function renderWeather(data) {
+    var elWeather = document.querySelector('.weather');
+    elWeather.querySelector('.weather-location').innerText = `${data.name}, ${data.weather[0].description}`;
+    elWeather.querySelector('.temp').innerText = `${data.main.temp} tempature, from ${data.main.temp_min} to ${data.main.temp_max}`;
+}
